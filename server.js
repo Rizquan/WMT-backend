b8ff877fdb24c57062615405f10b021b799d3b64
Rizquan Mohamed <mohammedrizquan10@gmail.com>
2026-04-24 23:29:52 +0530

    update in netlify

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://thriving-parfait-ebf7e6.netlify.app/"
+}));
 app.use(express.json());
 
 const authRoutes = require("./route");
